ca8dd49467be1029381c2461a8861c320112300b
Rosse Mary <78670466+RosseMry@users.noreply.github.com>
2026-07-29 11:06:41 +0000

    feat: 30days

---
+++ 30Days/day_1/helloworld.py
@@ -0,0 +1,16 @@
+import sys
+print(sys.version)
+
+print("\nOperations\n")
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("\nstrings\n")
+print("Name : Rosse")
+print("Family Name : Marcas")
+print("Country : Peru")
